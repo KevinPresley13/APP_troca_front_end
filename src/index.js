d5e1b8a1b06ea665916de5129fabdcc850c2f9c9
Kevin Presley <kevinpresley1995@gmail.com>
2023-04-06 20:26:33 -0300

    refatorando codigo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-let changeBtn = document.querySelectorAll('#button')
-let divCliente = document.querySelectorAll('.info')
-
-for(let i= 0; i< changeBtn.length;i++){
-    changeBtn[i].addEventListener("click", ()=> {
+let viewBtn = document.querySelectorAll('#button');
+let divCliente = document.querySelectorAll('.info');
+const dataFormat = (data)=>{
+    let newData = data;
+    return newData.split('-').reverse().join('/');
+};
+//mostra infotemaçoes da troca
+for(let i= 0; i< viewBtn.length;i++){
+    viewBtn[i].addEventListener("click", ()=> {
         divCliente[i].classList.toggle("v")
     });
-}
+    console.log(viewBtn[i])
+};
